ccd228e3a61a9ab2771a896e0795d530e6c7f789
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2023-01-16 10:57:30 +0800

    More mapping in AdminController

--- src/main/java/sda/project/SRAD/Controller/ApplicantController.java
+++ src/main/java/sda/project/SRAD/Controller/ApplicantController.java
@@ -13,8 +13,33 @@ public class ApplicantController {
         return "Applicant/ApplicantMenu";
     }
 
-    @GetMapping("/apply")
+    @GetMapping("/apply/form")
     public String apply() {
         return "Applicant/StudentRegistration";
+    }
+
+    @GetMapping("/apply/pay")
+    public String pay() {
+        return "Applicant/Payment";
+    }
+
+    @GetMapping("/apply/upload")
+    public String upload() {
+        return "Applicant/UploadDocuments";
+    }
+
+    @GetMapping("/view-initiated")
+    public String viewInitiated() {
+        return "Applicant/ViewInitiated";
+    }
+
+    @GetMapping("/view-status")
+    public String viewStatus() {
+        return "Applicant/ViewApplicationStat";
+    }
+
+    @GetMapping("/become-agents")
+    public String becomeAgents() {
+        return "Applicant/BecomeAgents";
     }
 }
--- src/main/java/sda/project/SRAD/Controller/StaffController.java
+++ src/main/java/sda/project/SRAD/Controller/StaffController.java
@@ -12,4 +12,19 @@ public class StaffController {
     public String index() {
         return "Staff/StaffMenu";
     }
+
+    @GetMapping("/student-applications")
+    public String studentApplications() {
+        return "Staff/StudentApplications";
+    }
+
+    @GetMapping("/agent-applications")
+    public String agentApplications() {
+        return "Staff/AgentApplications";
+    }
+
+    @GetMapping("/statistics")
+    public String statistics() {
+        return "Staff/Statistics";
+    }
 }
